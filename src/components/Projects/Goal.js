3ae66fc7d96dcd5415f6695a855c504bedfc1168
booyaChael <chaeeul2022@gmail.com>
2023-05-07 15:56:16 +0900

    header 완성

---
+++ src/components/Projects/Goal.js
@@ -0,0 +1,15 @@
+import classes from "./Goal.module.css";
+
+const Goal = (props) => {
+  return (
+    <section className={classes.goal}>
+      <h2>하루하루는 최선을 다해, 인생 전체는 되는대로</h2>
+      <ul>
+        <li>유데미 강의 마무리하기</li>
+        <li>프로젝트 준비하기</li>
+      </ul>
+    </section>
+  );
+};
+
+export default Goal;
