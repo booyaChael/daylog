c23f6f5c7bac583d5abeb179d3d8564699cec8b5
booyaChael <chaeeul2022@gmail.com>
2023-05-30 22:10:36 +0900

    localStorage에 todayContext 저장하기

---
+++ src/components/Today/Diary.js
@@ -0,0 +1,87 @@
+import classes from "./Diary.module.css";
+import { useRef, useState } from "react";
+
+const isEmpty = (value) => value.trim() === "";
+const isNumber0to100 = (value) => {
+  const number = Number(value);
+  return !isNaN(number) && number >= 0 && number <= 100;
+};
+
+const Diary = (props) => {
+  const [formInputValidity, setFormInputValidity] = useState({
+    dateIsValid: true,
+    scoreIsValid: true,
+    textIsValid: true,
+  });
+  console.log(formInputValidity);
+  const dateInputRef = useRef();
+  const scoreInputRef = useRef();
+  const textInputRef = useRef();
+  const currentDate = new Date().toISOString().split("T")[0];
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const enteredDate = dateInputRef.current.value;
+    const enteredScore = scoreInputRef.current.value;
+    const enteredText = textInputRef.current.value;
+
+    const enteredDateIsValid = !isEmpty(enteredDate);
+    const enteredScoreIsValid = isNumber0to100(enteredScore);
+    const enteredTextIsValid = !isEmpty(enteredText);
+
+    setFormInputValidity({
+      dateIsValid: enteredDateIsValid,
+      scoreIsValid: enteredScoreIsValid,
+      textIsValid: enteredTextIsValid,
+    });
+
+    const formIsValid =
+      enteredDateIsValid && enteredScoreIsValid && enteredTextIsValid;
+
+    if (!formIsValid) {
+      return;
+    }
+
+    props.onSave({
+      date: enteredDate,
+      score: enteredScore,
+      text: enteredText,
+    });
+  };
+  return (
+    <form className={classes.form} onSubmit={submitHandler}>
+      <div className={classes.control}>
+        <label>날짜</label>
+        <input
+          type="date"
+          defaultValue={currentDate}
+          ref={dateInputRef}
+        ></input>
+        {!formInputValidity.dateIsValid && <p>날짜를 입력해주세요</p>}
+      </div>
+      <div className={classes.control}>
+        <label>점수 (0~100)</label>
+        <input type="number" placeholder="0" ref={scoreInputRef}></input>
+        {!formInputValidity.scoreIsValid && (
+          <p>0에서 100 사이의 숫자로 입력해주세요</p>
+        )}
+      </div>
+      <div className={`${classes.control} ${classes.memo}`}>
+        <label>기록 몇 마디</label>
+        <textarea rows="3" ref={textInputRef}></textarea>
+        {!formInputValidity.textIsValid && (
+          <p>오늘에 대한 이야기를 간단히 적어주세요</p>
+        )}
+        <div className={classes.actions}>
+          <button type="button" onClick={props.onClose}>
+            뒤로
+          </button>
+          <button type="submit" className={classes.submit}>
+            저장
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+};
+export default Diary;
--- src/store/TodayProvider.js
+++ src/store/TodayProvider.js
@@ -1,38 +1,56 @@
 import TodayContext from "./today-context";
-import { useReducer } from "react";
+import { useReducer, useEffect } from "react";
 
-const defaultTodayState = {
-  projects: [],
-  totalTime: 0,
-};
-
+const defaultTodayState = JSON.parse(localStorage.getItem("todayData"));
 const todayReducer = (state, action) => {
   if (action.type === "ADD") {
     const existingProjectIndex = state.projects.findIndex(
       (project) => project.id === action.project.id
     );
-    console.log(existingProjectIndex);
-    const existingProject = state.projects[existingProjectIndex];
-    console.log(existingProject);
     let updatedProjects;
-    if (existingProjectIndex >= 0) {
-      console.log(action.project);
+    const updatedTime = state.totalTime + action.project.time;
+    if (existingProjectIndex !== -1) {
+      const existingProject = state.projects[existingProjectIndex];
       const updatedProject = {
         ...existingProject,
         time: existingProject.time + action.project.time,
       };
-      updatedProjects = state.projects;
+      updatedProjects = [...state.projects];
       updatedProjects[existingProjectIndex] = updatedProject;
     } else {
       updatedProjects = state.projects.concat(action.project);
     }
-    const updatedTime = state.totalTime + action.project.time;
-    console.log(updatedProjects);
+
     return {
       projects: updatedProjects,
       totalTime: updatedTime,
     };
   }
+
+  if (action.type === "REMOVE") {
+    const existingProjectIndex = state.projects.findIndex(
+      (project) => project.id === action.project.id
+    );
+    const existingProject = state.projects[existingProjectIndex];
+    let updatedProjects;
+    if (existingProject.time === 1) {
+      updatedProjects = state.items.filter(
+        (project) => project.id !== action.project.id
+      );
+    } else {
+      updatedProjects = [...state.projects];
+      updatedProjects[existingProjectIndex] = {
+        ...existingProject,
+        time: existingProject.time - 1,
+      };
+    }
+    const updatedTotalTime = state.totalTime - action.project.time;
+    return { projects: updatedProjects, totalTime: updatedTotalTime };
+  }
+  if (action.type === "CLEAR") {
+    return defaultTodayState;
+  }
+  return defaultTodayState;
 };
 
 const TodayProvider = (props) => {
@@ -40,16 +58,34 @@ const TodayProvider = (props) => {
     todayReducer,
     defaultTodayState
   );
+
+  useEffect(() => {
+    localStorage.setItem("todayData", JSON.stringify(todayState));
+
+    const todayDataFromLocalStorage = JSON.parse(
+      localStorage.getItem("todayData")
+    );
+    console.log(todayDataFromLocalStorage);
+  }, [todayState]);
+
   const addProjectHandler = (project) => {
     dispatchTodayAction({ type: "ADD", project });
   };
-  const removeProjectHandler = () => {};
+
+  const removeProjectHandler = (project) => {
+    dispatchTodayAction({ type: "REMOVE", project });
+  };
+
+  const clearProjectHandler = () => {
+    dispatchTodayAction({ type: "CLEAR" });
+  };
 
   const todayContext = {
     projects: todayState.projects,
     totalTime: todayState.totalTime,
     addProject: addProjectHandler,
     removeProject: removeProjectHandler,
+    clearProject: clearProjectHandler,
   };
 
   return (
